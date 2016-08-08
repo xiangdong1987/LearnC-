c1c2d7ae45cc9c0fd190944fb15964336b536310
xiangdong <xiangdongdong@chelun.com>
2016-08-08 16:48:36 +0800

    1.数据结构

---
+++ basic/struck.cpp
@@ -0,0 +1,62 @@
+//
+// Created by xiangdong on 16/8/8.
+//
+#include "MyHeader.h"
+
+//定义数据结构
+struct person {
+    char name[10];
+    int height;
+    int weight;
+};
+
+//计算标准体重
+void cale(person one) {
+    float normal;
+    normal = (one.height - 100) * 0.9;
+    if (normal > one.weight) {
+        cout << one.name << "的体重偏轻" << normal - one.weight << "kg" << endl;
+    }
+    if (normal < one.weight) {
+        cout << one.name << "您的体重超重" << one.weight - normal << "kg" << endl;
+    }
+    if (normal == one.weight) {
+        cout << one.name << "恭喜您体重很标准" << endl;
+    }
+}
+
+//计算标准体重
+void cale(person *one) {
+    float normal;
+    normal = (one->height - 100) * 0.9;
+    if (normal > one->weight) {
+        cout << one->name << "的体重偏轻" << normal - one->weight << "kg" << endl;
+    }
+    if (normal < one->weight) {
+        cout << one->name << "您的体重超重" << one->weight - normal << "kg" << endl;
+    }
+    if (normal == one->weight) {
+        cout << one->name << "恭喜您体重很标准" << endl;
+    }
+}
+
+int main() {
+    person me;
+    cout << "输入您的名字:" << endl;
+    cin >> me.name;
+    cout << "输入您的身高:" << endl;
+    cin >> me.height;
+    cout << "输入您的体重:" << endl;
+    cin >> me.weight;
+    cale(me);
+    //指针数据结构
+    person *her;
+    cout << "输入您的名字:" << endl;
+    cin >> her->name;
+    cout << "输入您的身高:" << endl;
+    cin >> her->height;
+    cout << "输入您的体重:" << endl;
+    cin >> her->weight;
+    cale(her);
+    return 0;
+}
